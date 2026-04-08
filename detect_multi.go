7aa3097f6aa7cf5c336ddfc873e1e2c285c06dd6
amirnsahmad <32689056+amirnsahmad@users.noreply.github.com>
2026-04-07 22:34:51 -0300

    HEAD adicionado nos metodos '

--- detect_multi.go
+++ detect_multi.go
@@ -65,7 +65,7 @@ func pushDetectJobs(targets []string, queue chan<- *DetectParams) {
 				continue
 			}
 			for _, pm := range PaddingMethods {
-				for _, rm := range []string{"POST", "OPTIONS", "GET"} {
+				for _, rm := range []string{"POST", "OPTIONS", "GET","HEAD"} {
 					variants := sm.GetVariants()
 					for _, v := range variants {
 						for _, target := range targets {
